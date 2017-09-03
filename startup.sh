7464365a386aa41f500ef29d1ff8d26aa57d7023
James Winegar <jameswine@gmail.com>
2017-09-03 03:48:29 -0500

    update startup for pyspark

--- startup.sh
+++ startup.sh
@@ -3,3 +3,4 @@
 echo "###################### Starting Jupyter Notebook ######################"
 source /opt/anaconda/bin/activate
 jupyter notebook --port 8889 --notebook-dir='/media/notebooks' --ip='*' --no-browser --allow-root &
+pyspark &
