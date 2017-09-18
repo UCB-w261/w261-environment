683872259781d55aaf962bfe93f9d02fbc99b45d
James Winegar <jameswine@gmail.com>
2017-09-18 11:03:29 -0500

    remove additional pyspark runner

--- start-notebook-python.sh
+++ start-notebook-python.sh
@@ -3,4 +3,3 @@
 echo "###################### Starting Jupyter Notebook ######################"
 source /opt/anaconda/bin/activate
 jupyter notebook --port 8889 --notebook-dir='/media/notebooks' --ip='*' --no-browser --allow-root &
-pyspark &
