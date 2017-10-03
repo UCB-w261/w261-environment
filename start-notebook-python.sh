ab0ebe497d617008f678e4b09067b2b6ebe4fe66
jwinegar <jwinegar@americanphysician.partners>
2017-10-03 18:34:47 -0500

    Add Altiscale Example notebook

--- start-notebook-python.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-echo "###################### Starting Jupyter Notebook ######################"
-source /opt/anaconda/bin/activate
-jupyter notebook --port 8889 --notebook-dir='/media/notebooks' --ip='*' --no-browser --allow-root &
