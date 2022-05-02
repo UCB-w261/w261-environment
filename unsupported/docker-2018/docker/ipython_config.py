20d1b6cca1037d494ca5ab32fe9e0e5ec928d06e
luisvillarreal <34928905+luisvillarreal@users.noreply.github.com>
2022-05-02 16:37:51 -0500

    Delete unsupported directory

--- unsupported/docker-2018/docker/ipython_config.py
+++
@@ -1,1 +0,0 @@
-c.InteractiveShellApp.exec_lines = ['import matplotlib.pyplot as plt','plt.style.use(\'seaborn-dark\')']
--- unsupported/docker-2018/docker/start-notebook.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-
-echo "###################### Starting Jupyter Notebook ######################"
-jupyter lab &
